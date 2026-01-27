0a58155deafbd07c50da8617b70e5db2e3fd6315
Nico1167 <nicolas.haushalter@ensta.fr>
2026-01-27 10:30:41 +0100

    début classe obstacle

--- trajectoire/include/trajectoire.hpp
+++ trajectoire/include/trajectoire.hpp
@@ -4,3 +4,47 @@
 #include <map>
 
 using namespace std;
+
+//outils vecteurs
+vector<double> operator+(const vector<double>& u, const vector<double>& v)
+{
+    int n=u.size();
+    vector<double> w(n);
+    for (int i=0;i<n; i++) 
+    {
+        w[i]=u[i]+v[i];
+    }
+    return w;
+}
+
+vector<double> operator-(const vector<double>& u, const vector<double>& v)
+{
+    int n=u.size();
+    vector<double> w(n);
+    for (int i=0;i<n; i++) 
+    {
+        w[i]=u[i]-v[i];
+    }
+    return w;
+}
+
+
+// classe obstacle
+class obstacle
+{
+    protected:
+    vector<vector<double>> sommets; //sommets dans le sens trigo
+    vector<vector<double>> normales; //normales des sommets SiSi+1
+    int nbsommets;
+    public :
+    obstacle (const vector<vector<double>>& u): sommets(u)
+    {
+        nbsommets = sommets.size();
+        for (int i = 0; i<nbsommets; ++i)
+        {
+            double x = sommets[i][0];
+            double y = sommets[i][1];
+            normales[i]
+        }
+    }
+}
