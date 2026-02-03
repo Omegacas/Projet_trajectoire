eec49a88b2c324e6c17c31c1bdd81658060eab23
Antoine LAGRANGE <antoinelagrange20@gmail.com>
2026-02-03 10:01:05 +0100

    dijkstra #1

--- trajectoire/include/dijkstra.hpp
+++ trajectoire/include/dijkstra.hpp
@@ -5,7 +5,35 @@
 
 
 
-vector<vector<double>> dijkstra(Graph* graph, int start_id, int end_id) {
+vector<vector<double>> dijkstra(vector<vector<double>> matrice_graphe, vector<double> depart, vector<double> arrivee) {
+    int n = matrice_graphe.size();
+    vector<double> tableau(n);
+    vector<double> parcours;
+    vector<double> etape = depart;
+
+    // Initialisation de tableau
+    for(int i =0; i<n; ++i){
+        if matrice(depart)(i) == 0{
+            tableau(i) = 999999;}// A remplacer par + infini pour la prochaine fois
+        else{
+            tableau(i) = matrice(depart)(i);
+        }
+        tableau(depart)(depart) = 0;
+
+    //Iteration
+    parcours.push_back(etape);
+    
+    
+
+
+
+        
+        
+    }
+
+
+
+
     
 }
-// retourne u n vecteur avec les sommets parcourus
+// retourne un vecteur avec les sommets parcourus
