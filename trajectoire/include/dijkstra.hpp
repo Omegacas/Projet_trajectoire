d7ff3b6b6444db1b70cc87c2086e767288240784
Antoine LAGRANGE <antoinelagrange20@gmail.com>
2026-02-10 09:54:11 +0100

    débogage de dijkstra

--- trajectoire/include/dijkstra.hpp
+++ trajectoire/include/dijkstra.hpp
@@ -3,44 +3,40 @@
 #include <vector>
 #include <map>
 
+using namespace std ; 
 
-
-vector<vector<double>> dijkstra(vector<vector<double>> matrice_graphe, int depart, int arrivee) {
+vector<int> dijkstra(vector<vector<double>> matrice_graphe, int depart, int arrivee) {
    int n = matrice_graphe.size();
 
     vector<double> tableau(n);
     vector<int> parcours;
     vector<int> visite(n);
-    vector<int> predecesseur(n)
-    vector<double> etape = depart;
-
-    if(depart == arrivee){
-        return{depart};
-    }
+    vector<int> predecesseur(n) ; 
+    int etape = depart;
 
     // Initialisation de tableau et visites
     for(int i =0; i<n; ++i){
-        visite(i) = 0;
-        predecesseur(i) = -1;
-        if matrice(depart)(i) == 0{
-            tableau(i) = 999999;}// A remplacer par + infini pour la prochaine fois
+        visite[i] = 0;
+        predecesseur[i] = -1;
+        if (matrice_graphe[depart][i] == 0){
+            tableau[i] = 999999;}// A remplacer par + infini pour la prochaine fois
         else{
-            tableau(i) = matrice(depart)(i);
+            tableau[i] = matrice_graphe[depart][i];
         }
-        tableau(depart) = 0;
+        tableau[depart] = 0;
 
     //Iteration
     int etape = depart;
     while (etape != -1 && etape != arrivee) {
-        visite(etape) = 1;
+        visite[etape] = 1;
         parcours.push_back(etape);
 
         for (int i = 0; i < n; i++) {
-            if (matrice_graphe(etape)(i) > 0 && visites(i)!=1) {
-                double nouvelle_dist = tableau(etape) + matrice_graphe(etape)(i);
-                if (nouvelle_dist < tableau(i)) {
-                    tableau(i) = nouvelle_dist;
-                    predecesseur(i) = etape;
+            if (matrice_graphe[etape][i] > 0 && visite[i]!=1) {
+                double nouvelle_dist = tableau[etape] + matrice_graphe[etape][i];
+                if (nouvelle_dist < tableau[i]) {
+                    tableau[i] = nouvelle_dist;
+                    predecesseur[i] = etape;
                 }
             }
         }
@@ -49,8 +45,8 @@ vector<vector<double>> dijkstra(vector<vector<double>> matrice_graphe, int depar
         double dist_min = 999999;
         int prochain_sommet = -1;
         for (int i = 0; i < n; i++) {
-            if (visite(i)!=1 && tableau(i) < dist_min) {
-                dist_min = tableau(i);
+            if (visite[i]!=1 && tableau[i] < dist_min) {
+                dist_min = tableau[i];
                 prochain_sommet = i;
             }
         }
